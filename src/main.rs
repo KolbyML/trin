2fbc01068d0fbc72cee8ac63de260365aa444ea6
Kolby Moroz Liebl <31669092+KolbyML@users.noreply.github.com>
2024-03-15 12:26:23 -0600

    patch

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,16 @@
 #![warn(clippy::unwrap_used)]
+use std::time::Duration;
 
 use ethportal_api::types::cli::TrinConfig;
 use tracing::error;
-use trin_utils::log::init_tracing_logger;
 
 use trin::run_trin;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    init_tracing_logger();
+    console_subscriber::ConsoleLayer::builder()
+        .retention(Duration::from_secs(60))
+        .init();
     let trin_config = TrinConfig::from_cli();
     let rpc_handle = run_trin(trin_config).await?;
 
